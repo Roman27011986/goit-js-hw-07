a41baf4710eb771c579813200905124f2d3a97a7
Roman27011986 <>
2020-11-15 01:24:53 +0200

    gdgf

--- task-1.js
+++ task-1.js
@@ -1,9 +1,9 @@
-const itemRef = document.querySelectorAll('#categories .item');
+// const itemRef = document.querySelectorAll('#categories .item');
 
-console.log(`в списке ${itemRef.length} категории`);
-const tittleRef = document.querySelectorAll('#categories .item');
-tittleRef.forEach(name => {
-  console.log(
-    `Категория: ${name.firstElementChild.textContent} - Количество элементов: ${name.lastElementChild.children.length}`,
-  );
-});
+// console.log(`в списке ${itemRef.length} категории`);
+// const tittleRef = document.querySelectorAll('#categories .item');
+// tittleRef.forEach(name => {
+//   console.log(
+//     `Категория: ${name.firstElementChild.textContent} - Количество элементов: ${name.lastElementChild.children.length}`,
+//   );
+// });
