7b25df0af820da4f1eed87f0bcfa0380d87e4e22
Roman27011986 <>
2020-11-15 12:17:49 +0200

    укеу

--- task-2.js
+++ task-2.js
@@ -7,16 +7,18 @@ const ingredients = [
   'Приправы',
 ];
 let ul = document.querySelector('#ingredients');
-let li1 = document.createElement('li');
-let li2 = document.createElement('li');
-let li3 = document.createElement('li');
-let li4 = document.createElement('li');
-let li5 = document.createElement('li');
+const F1 = 
 
-li1.textContent = ingredients[0];
-li2.textContent = ingredients[1];
-li3.textContent = ingredients[2];
-li4.textContent = ingredients[3];
-li5.textContent = ingredients[4];
+// let li1 = document.createElement('li');
+// let li2 = document.createElement('li');
+// let li3 = document.createElement('li');
+// let li4 = document.createElement('li');
+// let li5 = document.createElement('li');
 
-ul.append(li1, li2, li3, li4, li5);
+// li1.textContent = ingredients[0];
+// li2.textContent = ingredients[1];
+// li3.textContent = ingredients[2];
+// li4.textContent = ingredients[3];
+// li5.textContent = ingredients[4];
+
+// ul.append(li1, li2, li3, li4, li5);
